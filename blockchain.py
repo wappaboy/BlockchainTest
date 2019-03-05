bd13a13e84f616a263c8c87f5e9e24786f3cc428
wappaboy <shogo.mtsk@gmail.com>
2019-03-05 17:46:13 +0900

    blockchain.py 修正

--- blockchain.py
+++ blockchain.py
@@ -7,7 +7,7 @@ from urllib.parse import urlparse
 from uuid import uuid4
 
 import requests
-from flask import Flask, jsonify, requset
+from flask import Flask, jsonify, request
 
 class Blockchain:
     def __init__(self):
@@ -16,7 +16,7 @@ class Blockchain:
         self.nodes = set()
 
         #ジェネシスブロックを作る
-        self.new_block(previous_hash = 1, proof = 100)
+        self.new_block(previous_hash = '1', proof = 100)
 
     def register_node(self, address):
         """
@@ -80,9 +80,9 @@ class Blockchain:
         for node in neighbours:
             response = requests.get(f'http://{node}/chain')
 
-            if response.status_code =- 200:
+            if response.status_code == 200:
                 length = response.json()['length']
-                chain - response.json()['chain']
+                chain = response.json()['chain']
 
                 # 長さが長く、チェーンが有効かどうかチェックする
                 if length > max_length and self.valid_chain(chain):
@@ -135,36 +135,10 @@ class Blockchain:
 
         return self.last_block['index'] + 1
 
-    def proof_of_work(self, last_proof):
-        """
-        シンプルなプルーフ・オブ・ワークのアルゴリズム:
-         - hash(pp') の最初の4つが0となるような p' を探す
-         - p は1つ前のブロックのプルーフ、 p' は新しいブロックのプルーフ
-        :param last_proof: <int>
-        :return: <int>
-        """
-
-        last_proof = last_block['proof']
-        last_hash = self.hash(last_block)
-
-        proof = 0
-        while self.valid_proof(last_proof, proof) is False:
-            proof += 1
-
-        return proof
-
-    @staticmethod
-    def valid_proof(last_proof, proof):
-        """
-        プルーフが正しいかを確認する: hash(last_proof, proof)の最初の4つが0となっているか？
-        :param last_proof: <int> 前のプルーフ
-        :param proof: <int> 現在のプルーフ
-        :return: <bool> 正しければ true 、そうでなければ false
-        """
-
-        guess = f'{last_proof}{proof}'.encode()
-        guess_hash = hashlib.sha256(guess).hexdigest()
-        return guess_hash[:4] == "0000"
+    @property
+    def last_block(self):
+        # チェーンの最後のブロックをリターンする
+        return self.chain[-1]
 
     @staticmethod
     def hash(block):
@@ -178,116 +152,142 @@ class Blockchain:
         block_string = json.dumps(block, sort_keys = True).encode()
         return hashlib.sha256(block_string).hexdigest()
 
-    @property
-    def last_block(self):
-        # チェーンの最後のブロックをリターンする
-        return self.chain[-1]
 
-    # ノードを作る
-    # Flaskについてくわしくはこちらhttp://flask.pocoo.org/docs/0.12/quickstart/#a-minimal-application
-    app = Flask(__name__)
+    def proof_of_work(self, last_block):
+        """
+        シンプルなプルーフ・オブ・ワークのアルゴリズム:
+        - hash(pp') の最初の4つが0となるような p' を探す
+        - p は1つ前のブロックのプルーフ、 p' は新しいブロックのプルーフ
+        :param last_proof: <int>
+        :return: <int>
+        """
 
-    # このノードのグローバルにユニークなアドレスを作る
-    node_identifire = str(uuid4()).replace('-', '')
-
-    #ブロックチェーンクラスをインスタンス化する
-    blockchain = Blockchain()
-
-    #メソッドはPOSTで/transactions/newエンドポイントを作る。メソッドはPOSTなのでデータを送信する
-    @app.route('/transactions/new', method=['POST'])
-    def new_transaction():
-        values = request.get_json()
-
-        # POSTされたデータに必要なデータがあるかを確認
-        required = ['sender', 'recipient', 'amount']
-        if not all(k in values for k in required):
-            return 'Missing values', 400
-
-        #新しいトランザクションを作る
-        index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
-
-        response = {'message': f'トランザクションはブロック {index} に追加されました'}
-        return jsonify(response), 201
-
-    # メソッドではGETで/mineエンドポイントを作る
-    @app.route('/mine', methods=['GET'])
-    def mine():
-        # 次のプルーフを見つけるためプルーフ・オブ・ワークアルゴリズムを使用する
-        last_block - blockchain.last_block
         last_proof = last_block['proof']
-        proof = blockchain.proof_of_work(last_proof)
+        last_hash = self.hash(last_block)
 
-        # プルーフを見つけたことに対する報酬を得る
-        # 送信者は、採掘者が新しいコインを採掘したことを表すために"0"とする
-        blockchain.new_transaction(
-            sender = "0",
-            recipient = node_identifire,
-            amount = 1,
-        )
+        proof = 0
+        while self.valid_proof(last_proof, proof, last_hash) is False:
+            proof += 1
 
-        # チェーンに新しいブロックを加えることで、新しいブロックを採掘する
-        previous_hash = blockchain.hash(last_block)
-        block = blockchain.new_block(proof)
+        return proof
 
+    @staticmethod
+    def valid_proof(last_proof, proof, last_hash):
+        """
+        プルーフが正しいかを確認する: hash(last_proof, proof)の最初の4つが0となっているか？
+        :param last_proof: <int> 前のプルーフ
+        :param proof: <int> 現在のプルーフ
+        :return: <bool> 正しければ true 、そうでなければ false
+        """
+
+        guess = f'{last_proof}{proof}{last_hash}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
+
+# ノードを作る
+# Flaskについてくわしくはこちらhttp://flask.pocoo.org/docs/0.12/quickstart/#a-minimal-application
+app = Flask(__name__)
+
+# このノードのグローバルにユニークなアドレスを作る
+node_identifire = str(uuid4()).replace('-', '')
+
+#ブロックチェーンクラスをインスタンス化する
+blockchain = Blockchain()
+
+# メソッドではGETで/mineエンドポイントを作る
+@app.route('/mine', methods=['GET'])
+def mine():
+    # 次のプルーフを見つけるためプルーフ・オブ・ワークアルゴリズムを使用する
+    last_block = blockchain.last_block
+    proof = blockchain.proof_of_work(last_block)
+
+    # プルーフを見つけたことに対する報酬を得る
+    # 送信者は、採掘者が新しいコインを採掘したことを表すために"0"とする
+    blockchain.new_transaction(
+        sender = "0",
+        recipient = node_identifire,
+        amount = 1,
+    )
+
+    # チェーンに新しいブロックを加えることで、新しいブロックを採掘する
+    previous_hash = blockchain.hash(last_block)
+    block = blockchain.new_block(proof, previous_hash)
+
+    response = {
+        'message': '新しいブロックを採掘しました',
+        'index': block['index'],
+        'transactions': block['transactions'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash'],
+    }
+    return jsonify(response), 200
+
+#メソッドはPOSTで/transactions/newエンドポイントを作る。メソッドはPOSTなのでデータを送信する
+@app.route('/transactions/new', methods=['POST'])
+def new_transaction():
+    values = request.get_json()
+
+    # POSTされたデータに必要なデータがあるかを確認
+    required = ['sender', 'recipient', 'amount']
+    if not all(k in values for k in required):
+        return 'Missing values', 400
+
+    #新しいトランザクションを作る
+    index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
+
+    response = {'message': f'トランザクションはブロック {index} に追加されました'}
+    return jsonify(response), 201
+
+# メソッドはGETで、ブロックチェーンをリターンする/chainエンドポイントを作る
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain),
+    }
+    return jsonify(response), 200
+
+@app.route('/nodes/resister', methods = ['POST'])
+def register_nodes():
+    values = request.get_json()
+
+    nodes = values.get('nodes')
+    if nodes is None:
+        return "Error: Please supply a valid list of nodes", 400
+
+    for node in nodes:
+        blockchain.register_node(node)
+
+    response = {
+        'message': 'New nodes have been added',
+        'total_nodes': list(blockchain.nodes),
+    }
+    return jsonify(response), 201
+
+@app.route('/nodes/resolve', methods=['GET'])
+def consensus():
+    replaced = blockchain.resolve_conflicts()
+
+    if replaced:
         response = {
-            'message': '新しいブロックを採掘しました',
-            'index': block['index'],
-            'transactions': block['transactions'],
-            'proof': block['proof'],
-            'previous_hash': block['previous_hash'],
+            'message': 'Our chain was replaced',
+            'new_chain': blockchain.chain
         }
-        return jsonify(response), 200
-
-    # メソッドはGETで、ブロックチェーンをリターンする/chainエンドポイントを作る
-    @app.route('/chain', methods=['GET'])
-    def full_chain():
+    else:
         response = {
-            'chain': blockchain.chain,
-            'length': len(blockchain.chain),
+            'message': 'Our chain is authoritative',
+            'chain': blockchain.chain
         }
-        return jsonify(response), 200
 
-    @app.route('/nodes/resister', methods = ['POST'])
-    def resister_nodes():
-        values = request.get_json()
+    return jsonify(response), 200
 
-        nodes = values.get('nodes')
-        if nodes is None:
-            return "Error: Please supply a valid list of nodes", 400
+# port5000でサーバーを起動する
+if __name__ == '__main__':
+    from argparse import ArgumentParser
 
-        for node in nodes:
-            blockchain.resister_node(node)
+    parser = ArgumentParser()
+    parser.add_argument('-p', '--port', default=500, type=int, help='port to listen on')
+    args = parser.parse_args()
+    port = args.port
 
-        response = {
-            'message': 'New nodes have been added',
-            'total_nodes': list(blockchain.nodes),
-        }
-        return jsonify(response), 201
-
-    @app.route('/nodes/resolve', methods=['GET'])
-    def consensus():
-        replaced = blockchain.resolve_conflicts()
-
-        if replaced:
-            response = {
-                'message': 'Our chain was replaced',
-                'new_chain': blockchain.chain
-            }
-        else:
-            response = {
-                'message': 'Our chain is authoritative',
-                'chain': blockchain.chain
-            }
-
-        return jsonify(response), 200
-
-    # port5000でサーバーを起動する
-    if __name__ =- '__main__':
-        from argparse import ArgumentParser
-
-        parser = ArgumentParser()
-        parser.add_argument('-p', '--port', default=500, type=int, help='port to listen on')
-        args = parser.parse_args()
-        port = args.port
-
-        app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
